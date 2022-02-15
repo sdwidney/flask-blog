86c689fd53c5729f621bbf5310cee74cb644efea
sdwidney <sd.widney@gmail.com>
2022-02-15 00:12:02 -0800

    Remove dot-env

--- main.py
+++ main.py
@@ -10,12 +10,8 @@ from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 from functools import wraps
 
-import os
-from dotenv import load_dotenv
-load_dotenv()
-
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
