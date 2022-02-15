53feaad96b5d711a29968d6ff527fe2c8d627bc3
sdwidney <sd.widney@gmail.com>
2022-02-14 21:55:49 -0800

    Add dotfiles

--- main.py
+++ main.py
@@ -10,8 +10,12 @@ from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 from functools import wraps
 
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
